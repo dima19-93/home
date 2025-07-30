aaded2a5d0ba56658661a9a6d0780dfba18dce89
dima19-93 <dima70593@ukr.net>
2025-07-30 23:13:24 +0300

    Update version.sh сomments
    add comments

--- version.sh
+++ version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Отримання версії операційної системи
 version=`lsb_release -a`
 echo "$version"
-
+# Список користувачів, які використовують bash shell
 users=`grep /bash /etc/passwd`
 echo "$users"
-
+# Відображення відкритих портів
 port=`ss -tlpn`
 echo "$port"
